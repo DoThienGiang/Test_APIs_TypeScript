db95c305e020fc75f5568761ff41d9f99ae82381
Đỗ Thiên Giang <dothiengiang1994@gmail.com>
2022-07-15 09:01:11 +0700

    up

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config'
-import app from './app';
-// const PORT = 3000;
-
-app.listen(process.env.PORT, () => {
-    console.log('Express server listening on port ' + process.env.PORT);
-})
